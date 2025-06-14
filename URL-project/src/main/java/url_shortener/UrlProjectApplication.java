2cd3f6e690fa76ba394273a4f87973662aed01b3
KellyJusthine <kellyjusthine2003@gmail.com>
2025-06-14 18:44:37 -0300

    Initial commit

---
+++ URL-project/src/main/java/url_shortener/UrlProjectApplication.java
@@ -0,0 +1,13 @@
+package url_shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlProjectApplication.class, args);
+	}
+
+}
